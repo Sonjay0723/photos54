9045910b97e8f683aef7c413b16e34cc30ec20f9
dhazari <dhrishtihazari@gmail.com>
2020-04-09 22:23:35 -0400

    UserController Complete

---
+++ src/controller/SearchController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.util.ArrayList;
+
+import javafx.stage.Stage;
+import model.User;
+
+public class SearchController {
+	
+	public Stage primaryStage;
+	
+	public void start(Stage primaryStage, User currUser, ArrayList<User> userList) {
+		
+		this.primaryStage = primaryStage;
+		
+	}
+
+}
--- src/model/Album.java
+++ src/model/Album.java
@@ -1,9 +1,12 @@
 package model;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.Objects;
 
 import javafx.collections.FXCollections;
@@ -29,6 +32,10 @@ public class Album implements Serializable {
 	
 	public ArrayList<Picture> getPictureList(){
 		return pictureList;
+	}
+	
+	public void setList(ArrayList<Picture> newPictureList) {
+		this.pictureList = newPictureList;
 	}
 	
 	public void addPicture(Picture newPicture) {
@@ -80,6 +87,35 @@ public class Album implements Serializable {
 	}
 	
 	public String toString() {
-		return this.getTitle();
+		String ret = this.getTitle();
+		ret += "\n"+Integer.toString(this.pictureList.size())+" Photos";
+		ret += "\nDate Range:\n";
+
+		String pattern = "MM/dd/yyyy HH:mm:ss";
+		DateFormat df = new SimpleDateFormat(pattern);
+		Date maxDate;
+		Date minDate;
+		String maxDateS = "_";
+		String minDateS = "_";
+		if(this.pictureList.size()>0) {
+			Date thisDate = this.pictureList.get(0).getDate().getTime();
+			maxDate = thisDate;
+			minDate = thisDate;
+			minDateS = df.format(thisDate);
+			maxDateS = minDateS;
+			for(int i=0; i<this.pictureList.size(); i++) {
+				if(this.pictureList.get(i).getDate().getTime().compareTo(maxDate)>0) {
+					maxDate = this.pictureList.get(i).getDate().getTime();
+					maxDateS = df.format(maxDate);
+				}
+				else if(this.pictureList.get(i).getDate().getTime().compareTo(minDate)<0){
+					minDate = this.pictureList.get(i).getDate().getTime();
+					minDateS = df.format(minDate);
+				}
+			}
+		}
+		ret+=minDateS+" - "+maxDateS;
+		
+		return ret;
 	}
 }
